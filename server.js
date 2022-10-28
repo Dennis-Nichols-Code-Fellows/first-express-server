619b6b31caf443d9b66c9a708697c7f889d62000
Dennis Nichols <dgnichols22@gmail.com>
2022-10-28 18:35:22 -0300

    added cache

--- server.js
+++ server.js
@@ -1,49 +1,36 @@
-'use strict';
+"use strict";
 
-console.log('Hello World!');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const getWeather = require('./weather.js');
+const getMovies = require('./movies.js');
 
-// -------- REQUIRES ----------
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const { request } = require('express');
-const getWeather = require('./weather');
-const getMovies = require('./movies');
-//this is our server
+
 const app = express();
-
-//this is middleware to share resources accross the web
 app.use(cors());
-
-// define port, if server on 3002 then I know something up on env file
 const PORT = process.env.PORT || 3002;
-// -------- ENDPOINTS ----------
+// ----End points ------
 
-// Base endpoint
-app.get('/', (req, res) =>{
-  console.log('This is showing up!');
-  res.status(200).send('Welcome to the server. Beep Boop Beep.');
+app.get("/", (req, res) => {
+  console.log("This is showing up!");
+  res.status(200).send("Welcome to the server. Beep Boop Beep.");
 });
 
-// weather endpoint
-app.get('/weather', getWeather);
+app.get("/movies", getMovies);
 
+app.get("/weather", weatherHandler);
 
-app.get('/movies',getMovies);
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  getWeather(lat, lon)
+    .then((summaries) => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send("Sorry. Something went wrong!");
+    });
+}
 
-app.get('*', (req, res) => {
-  res.status(404).send('This route does not exist');
-});
-
-
-// -------- ERROR HANDLING ----------
-
-app.use((error, request, response, next)=> {
-  response.status(500).send(error.message);
-});
-
-
-
-// -------- SERVER START ----------
-
-app.listen(PORT, () => console.log(`We are running on port ${PORT}.`));
+app.listen(PORT, () =>
+  console.log(`Server up on ${PORT}`)
+);
